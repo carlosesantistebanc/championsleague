f3a6a1218e8f1515e76e3af44d1feca43f82f352
Carlos <santistebancalzadoc@gmail.com>
2022-03-18 23:14:09 -0500

    change in 4 files, changes font style and sub navbar, finally it's responsive

---
+++ script.js
@@ -0,0 +1,9 @@
+//toggle menu/navbar script
+$(".menu-btn").click(function () {
+  $(".navbar .menu").toggleClass("active");
+  $(".menu-btn i").toggleClass("active");
+});
+
+//changing teams names
+/* var club = document.getElementById("mancity");
+club.innerHTML = "MCI"; */
